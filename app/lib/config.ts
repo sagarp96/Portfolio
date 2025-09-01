0d809f39792754cfff118171139d789fbff82a41
sagarp96 <sagarcool2345@gmail.com>
2025-09-01 16:52:11 +0530

    Blog Added

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,13 +1,12 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
+  baseUrl: "https://sagarpanwar.vercel.app/",
   title: "Sagar Panwar",
   name: "Sagar Panwar",
-  ogImage: "/opengraph-image.png",
-  description: "Hi, I'm Sagar Panwar, a passionate developer.",
+  ogImage: "/fevicon.ico",
+  description: "Sagar Panwar",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
   github: "https://github.com/sagarp96",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/in/sagar-panwar-2b81aa381/",
